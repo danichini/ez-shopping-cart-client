2948e2861929923e9ff358e183b883c4068a7059
Daniel Angel <daniel.reverol@gmail.com>
2024-05-03 11:38:19 +0100

    no logos applied on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Guitar from "./components/Guitar";
 import Header from "./components/Header";
 import { db } from "./data/db";
 
 function App() {
-  const [data, setData] = useState(db);
-  const [cart, setCart] = useState([]);
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+
+  const [data] = useState(db);
+  const [cart, setCart] = useState(initialCart);
+
+  const MAX_ITEMS = 5;
+
+  useEffect(() => {
+    saveLocalStorage();
+  }, [cart]);
 
   function addToCart(guitar) {
     const itemExists = cart.findIndex(({ id }) => guitar.id === id);
@@ -20,9 +31,47 @@ function App() {
     }
   }
 
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((item) => id !== item.id));
+  }
+
+  function incleaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if ((item.id === id) & (item.quantity < MAX_ITEMS)) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if ((item.id === id) & (item.quantity > 1)) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function clearCart() {
+    setCart([]);
+  }
+
+  function saveLocalStorage() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
   return (
     <>
-      <Header cart={cart} />
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        incleaseQuantity={incleaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+      />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 
-export default function Header({ cart }) {
+export default function Header({ cart, removeFromCart, incleaseQuantity, decreaseQuantity, clearCart }) {
   const cartTotal = useMemo(
     () =>
       cart.reduce(
@@ -16,7 +16,7 @@ export default function Header({ cart }) {
         <div className="row justify-content-center justify-content-md-between">
           <div className="col-8 col-md-3">
             <a href="index.html">
-              <img className="img-fluid" src="/img/logo.svg" alt="image logo" />
+              {/* <img className="img-fluid" src="/img/logo.svg" alt="image logo" /> */}
             </a>
           </div>
           <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
@@ -52,16 +52,20 @@ export default function Header({ cart }) {
                             <td>SRV</td>
                             <td className="fw-bold">${guitar.price}</td>
                             <td className="flex align-items-start gap-4">
-                              <button type="button" className="btn btn-dark">
+                              <button type="button" className="btn btn-dark" onClick={() => decreaseQuantity(guitar.id)}>
                                 -
                               </button>
                               {guitar.quantity}
-                              <button type="button" className="btn btn-dark">
+                              <button type="button" className="btn btn-dark" onClick={() => incleaseQuantity(guitar.id)}>
                                 +
                               </button>
                             </td>
                             <td>
-                              <button className="btn btn-danger" type="button">
+                              <button
+                                className="btn btn-danger"
+                                type="button"
+                                onClick={() => removeFromCart(guitar.id)}
+                              >
                                 X
                               </button>
                             </td>
@@ -75,10 +79,10 @@ export default function Header({ cart }) {
                     </p>
                   </>
                 ) : (
-                  <p className="text-center">El carrito esta vacio</p>
+                  <p className="text-center">Cart is empty</p>
                 )}
 
-                <button className="btn btn-dark w-100 mt-3 p-2">
+                <button className="btn btn-dark w-100 mt-3 p-2" onClick={clearCart}>
                   Empty Cart
                 </button>
               </div>
